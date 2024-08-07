006d07f1dec913d50a13367187a9f3825583d335
Jerry Malloney <jerrymalloney@gmail.com>
2024-08-07 00:17:25 -0400

    created image entity

---
+++ src/domain/entities/image.entity.ts
@@ -0,0 +1,41 @@
+import { CustomError } from "../errors/custom.error";
+
+export class Image {
+  constructor(
+    public id: number,
+    public externalId: string,
+    public name: string,
+    public alt: string,
+    public url: string,
+    public fileType: string,
+    public size: number
+  ) {}
+
+  fromObject(object: { [key: string]: any }): Image {
+    const { id, externalId, name, alt, url, fileType, size } = object;
+
+    if (!id) {
+      throw CustomError.badRequest("Missing id");
+    }
+    if (!externalId) {
+      throw CustomError.badRequest("Missing externalId");
+    }
+    if (!name) {
+      throw CustomError.badRequest("Missing name");
+    }
+    if (!alt) {
+      throw CustomError.badRequest("Missing alt");
+    }
+    if (!url) {
+      throw CustomError.badRequest("Missing url");
+    }
+    if (!fileType) {
+      throw CustomError.badRequest("Missing fileType");
+    }
+    if (!size) {
+      throw CustomError.badRequest("Missing size");
+    }
+
+    return new Image(id, externalId, name, alt, url, fileType, size);
+  }
+}
