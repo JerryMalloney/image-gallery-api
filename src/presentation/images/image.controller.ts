3128b4c8cc74d6b555d9b622c000b392a0b85d83
Jerry Malloney <jerrymalloney@gmail.com>
2024-08-10 03:24:17 -0400

    improved saveImage method to get the file name in a better way

--- src/presentation/images/image.controller.ts
+++ src/presentation/images/image.controller.ts
@@ -1,5 +1,6 @@
 import cloudinary from "cloudinary";
 import { Request, Response } from "express";
+import path from "path";
 import { envs } from "../../config/envs";
 import { SaveImageStorageDto } from "../../domain/dtos/image/save-image-storage-dto";
 import { ErrorHandler } from "../shared/errorHandler";
@@ -18,8 +19,7 @@ export class ImageController {
 
   saveImage = (req: Request, res: Response) => {
     const files = req.files as Express.Multer.File[];
-    const fileName = files[0].originalname.split(".").at(0);
-
+    const fileName = path.parse(files[0].originalname).name;
     const [error, imageDto] = SaveImageStorageDto.create({
       buffer: files[0].buffer,
       name: fileName,
@@ -30,15 +30,5 @@ export class ImageController {
       .saveImage(imageDto!)
       .then((result) => res.status(200).json(result))
       .catch((error) => ErrorHandler.handle(error, res, this.errorSource));
-
-    // const result = await new Promise((resolve) => {
-    //   cloudinary.v2.uploader
-    //     .upload_stream({ filename_override: "test" }, (error, uploadResult) => {
-    //       return resolve(uploadResult);
-    //     })
-    //     .end(files[0].buffer);
-    // });
-    // console.log(result as cloudinary.UploadApiResponse);
-    // console.log("working");
   };
 }
